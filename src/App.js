05886d15deddbabbbb312959034d1eef9a0640d0
MawatariTanaka <buinhatminh2005@gmail.com>
2022-03-27 14:03:11 +0700

    init commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './bundles/css/bootstrap.min.css';
+import './bundles/css/responsive.css';
+import './App.css';
+import './Header/Header.module.css';
+import Header from './Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
