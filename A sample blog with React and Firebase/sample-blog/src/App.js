d9edd6e93a7158cc0f8bf20d5c77e613a8db2e72
MawatariTanaka <buinhatminh2005@gmail.com>
2022-05-11 16:45:53 +0700

    Update post

--- A sample blog with React and Firebase/sample-blog/src/App.js
+++ A sample blog with React and Firebase/sample-blog/src/App.js
@@ -1,12 +1,40 @@
-import './App.css';
+import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { signOut } from 'firebase/auth';
+import { auth } from './firebaseConfig';
+import './App.css';
 import Home from './pages/Home';
+import CreatePost from './pages/CreatePost';
+import Login from './pages/Login';
 
 function App() {
+    const [isAuth, setIsAuth] = useState(false);
+
+    const signUserOut = () => {
+        signOut(auth).then(() => {
+            localStorage.clear();
+            setIsAuth(false);
+            window.location.pathname = '/';
+        });
+    };
     return (
         <Router className="App">
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/create-post">Create</Link>
+                {!isAuth ? (
+                    <Link to="/login">Login</Link>
+                ) : (
+                    <button onClick={signUserOut}>Log out</button>
+                )}
+            </nav>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/create-post" element={<CreatePost />} />
+                <Route
+                    path="/login"
+                    element={<Login setIsAuth={setIsAuth} />}
+                />
             </Routes>
         </Router>
     );
--- A sample blog with React and Firebase/sample-blog/src/pages/CreatePost.js
+++ A sample blog with React and Firebase/sample-blog/src/pages/CreatePost.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
 function CreatePost() {
-    return <div>CreatePost</div>;
+    return (
+        <div className="createPostPage">
+            <div className="cpContainer">
+                <h1>Create A Post</h1>
+                <div className="inputGp">
+                    <label>Title:</label>
+                    <input type="text" placeholder="Title" />
+                </div>
+                <div className="inputGp">
+                    <label>Post:</label>
+                    <textarea placeholder="Post" />
+                </div>
+                <button>Send my post</button>
+            </div>
+        </div>
+    );
 }
 
 export default CreatePost;
